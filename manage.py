3c02867185a20f1291b79d5513b7b1869fa7cb15
sinaastani <sinaastani@gmail.com>
2019-04-30 13:37:22 -0700

    add appt model, cleanup code/remove duplicate methods

--- manage.py
+++ manage.py
@@ -7,11 +7,12 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drchrono.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
-    from drchrono.endpoints import DoctorEndpoint
+    
+    from drchrono.endpoints import DoctorEndpoint, PatientEndpoint
     from drchrono.views import DoctorWelcome
     token = DoctorWelcome().get_token()
     d = DoctorEndpoint(token).get_doctor()
-    print(d)
+    p = PatientEndpoint(token).get_patients(d)
+    
 
 
